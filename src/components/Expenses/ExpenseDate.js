5d2f7e14e76e2caa49b70bf619290d583c582913
Niziol <niziol.luk@gmail.com>
2023-05-09 15:07:33 +0200

    Finished Expense logging app

---
+++ src/components/Expenses/ExpenseDate.js
@@ -0,0 +1,18 @@
+import './ExpenseDate.css';
+
+const ExpenseDate = (props) => {
+	const date = props.date;
+	const year = date.toLocaleDateString('en-US', { year: 'numeric' });
+	const month = date.toLocaleDateString('en-US', { month: 'long' });
+	const day = date.toLocaleDateString('en-US', { day: 'numeric' });
+
+	return (
+		<div className="expense-date">
+			<div className="expense-date__month">{month}</div>
+			<div className="expense-date__year">{year}</div>
+			<div className="expense-date__day">{day}</div>
+		</div>
+	);
+};
+
+export default ExpenseDate;
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
+
+const Expenses = (props) => {
+	const [filteredYear, setFilterYear] = useState('2020');
+
+	const filteredExpenses = props.expenses.filter(
+		(expense) =>
+			filteredYear === 'all' ||
+			expense.date.getFullYear().toString() === filteredYear
+	);
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilterYear(selectedYear);
+	};
+
+	return (
+		<Card className="expenses">
+			<ExpenseFilter
+				selected={filteredYear}
+				onChangeFilter={filterChangeHandler}
+			/>
+			<ExpensesChart expenses={filteredExpenses} />
+			<ExpensesList items={filteredExpenses} />
+		</Card>
+	);
+};
+
+export default Expenses;
